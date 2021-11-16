d26401614fa85632991ab1f1bffa25011bdac330
Ben <benjaminjwalker2910@gmail.com>
2021-11-16 16:04:51 +0000

    snap v4.1.1 - Removed unneccessary code comments

--- exercises/java/src/main/java/cards/MixedDeck.java
+++ exercises/java/src/main/java/cards/MixedDeck.java
@@ -4,16 +4,6 @@ import java.util.List;
 
 public class MixedDeck extends Deck {
     MixedDeck() {
-        super();
-        // for (Animal animal : Animal.values()) {
-        // cardsInDeck.add(new AnimalCard(animal));
-        // cardsInDeck.add(new AnimalCard(animal));
-        // }
-        // for (int suit = 0; suit < 4; suit++) {
-        // for (int faceValue = 0; faceValue < 13; faceValue++) {
-        // cardsInDeck.add(new PlayingCard(suit, faceValue));
-        // }
-        // }
         AnimalDeck aniDeck = new AnimalDeck();
         List<Card> animalDeck = aniDeck.getDeck();
         PlayingCardDeck pcDeck = new PlayingCardDeck();
